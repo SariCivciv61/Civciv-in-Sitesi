c282a440ee0361fc3057b1e04e5126640f9150b5
SariCivciv61 <ahmetalpalbayrak@gmail.com>
2026-09-21 21:56:00 +0300

    Add toggleMenu function for menu and overlay

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.getElementById('yanMenu');
+    const overlay = document.getElementById('overlay');
+
+    menu.classList.toggle('acik');
+    overlay.classList.toggle('acik');
+}
